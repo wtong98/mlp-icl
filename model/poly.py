682ad9aa73f9813d668adbf5c7d8fb87b8c2ce14
William Tong <williamtong105@gmail.com>
2023-11-13 14:02:27 -0500

    Delete schemes that didn't work

--- model/poly.py
+++ model/poly.py
@@ -28,61 +28,6 @@ class PolyNet(nn.Module):
 
     config: PolyConfig
 
-    def _fwd_product_only_positive(self, x):
-        z_kernel = self.param(f'z_kernel', nn.initializers.lecun_normal(), (x.shape[-1], 1))
-        z = jnp.log(jnp.abs(x))
-        z = z @ z_kernel
-        z = jnp.exp(z)
-        return z.flatten()
-    
-    def _fwd_product_only_cos_negative(self, x):
-        z_kernel = self.param(f'z_kernel', nn.initializers.lecun_normal(), (x.shape[-1], 1))
-        z = jnp.log(jnp.abs(x))
-        z = z @ z_kernel
-        neg_powers = z_kernel.T * (x < 0).astype(int)
-        sign = jnp.cos(jnp.pi * jnp.sum(neg_powers, axis=1))
-        z = jnp.exp(z) * jnp.expand_dims(sign, 1)  # NOTE: cosine generalization seems to be ineffectual
-        return z.flatten()
-    
-    def _fwd_product_mlp_parallel_cos(self, x):
-        if self.n_hidden % 2 != 0:
-            raise ValueError(f'n_hidden should be even, got n_hidden={self.n_hidden}')
-
-        proj_dim = self.config.n_hidden // 2
-
-        for i in range(self.config.n_layers):
-            x_lin = nn.Dense(proj_dim)(x)
-            z = jnp.log(jnp.abs(x))
-
-            z_kernel = self.param(f'z_kernel_{i}', nn.initializers.lecun_normal(), (x.shape[-1], proj_dim))
-            z_bias = self.param(f'z_bias_{i}', nn.initializers.zeros_init(), (proj_dim,))
-
-            z = z @ z_kernel + z_bias
-            neg_powers = z_kernel.T * (x < 0).astype(int)
-            sign = jnp.cos(jnp.pi * jnp.sum(neg_powers, axis=1))
-            z = jnp.exp(z) * jnp.expand_dims(sign, 1)
-            x = jnp.concatenate((x_lin, z), axis=-1)
-    
-        out = nn.Dense(1)(x).flatten()
-        return out
-    
-    def _fwd_product_mlp_seq_positive(self, x):
-        # NOTE: fails to learn anything meaningful, particularly on negative domain
-        hid = self.config.n_hidden
-
-        x = nn.Dense(hid)(x)
-        x = nn.relu(x) + 1e-8
-
-        x = jnp.log(x)
-        x = nn.Dense(hid)(x)
-        x = jnp.exp(x)
-
-        x = nn.Dense(hid)(x)
-        x = nn.relu(x)
-
-        x = nn.Dense(1)(x).flatten()
-        return x
-
     def m_block(self, x, n_features=None):
         if n_features is None:
             n_features = self.config.n_hidden
@@ -93,18 +38,11 @@ class PolyNet(nn.Module):
 
         beta = 10 # NOTE: sharpen tanh, can make trainable
         s = nn.tanh(beta * x)
-
-        # NOTE: new product feature style
         s_prod = jnp.einsum('ij,ik->ijk', s, s).reshape(s.shape[0], -1)
+
         s = jnp.concatenate((s, s_prod), axis=-1)
         s = nn.Dense(n_features, name='SignOut')(s)
         s = nn.tanh(s)
-
-        # NOTE: old 2-layer NN style
-        # s = nn.Dense(self.config.n_hidden, name='SignHid')(s)  # TODO: make separate param
-        # s = nn.gelu(s)
-        # s = nn.Dense(n_features, name='SignOut')(s)
-        # s = nn.tanh(0.5 * s) # NOTE: can also add sigmoid term for zeroing
 
         out = s * z
         return out
--- task/function.py
+++ task/function.py
@@ -37,6 +37,21 @@ class DotProductTask:
         return self
 
 
+class AttentionTask:
+    def __init__(self, domain, n_dims=5, batch_size=32):
+        self.lower, self.upper = domain
+        self.n_dims = n_dims
+        self.batch_size = batch_size
+    
+    def __next__(self):
+        xs = np.random.uniform(self.lower, self.upper, size=(self.batch_size, 2, self.n_dims))
+        ys = np.diag(xs[:,0] @ xs[:,1].T)
+        return xs, ys
+    
+    def __iter__(self):
+        return self
+    
+
 if __name__ == '__main__':
     task = DotProductTask(domain=(-5, 5))
     xs, ys = next(iter(task))
